9df3e7690145a8a48e6bb1e6344f59066c8dafdc
NicolasRaf <nicolasalves136@gmail.com>
2024-12-12 13:22:53 -0300

    Fix: gitignore build

--- Atividade-5.3/build/classes/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Client = exports.Bank = exports.Account = void 0;
-var account_1 = require("./account");
-Object.defineProperty(exports, "Account", { enumerable: true, get: function () { return account_1.Account; } });
-var bank_1 = require("./bank");
-Object.defineProperty(exports, "Bank", { enumerable: true, get: function () { return bank_1.Bank; } });
-var client_1 = require("./client");
-Object.defineProperty(exports, "Client", { enumerable: true, get: function () { return client_1.Client; } });
